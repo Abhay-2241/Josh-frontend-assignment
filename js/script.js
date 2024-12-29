6c570feae38409d9efb60c85fddd38d4f464fe0c
abhay das <dasabhay215@gmail.com>
2024-12-29 17:47:48 +0530

    implemented not scroll function when modals are open

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 // Cart Modal
 const mymodal = document.getElementById("myModal");
-// const modal = document.getElementById("hid");
-// const body = document.querySelector("body"); 
-// const showModal = function (e) { 
 
-//     mymodal.classList.toggle("hidden"); 
-//     if (!mmodal.classList.contains("hidden")) { 
-
-//         // Disable scroll 
-
-//         body.style.overflow = "hidden"; 
-
-//     } else { 
-
-//         // Enable scroll 
-
-//         body.style.overflow = "auto"; 
-
-//     } 
-
-// }; 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -29,11 +10,13 @@ var btn = document.getElementById("myBtn");
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   mymodal.style.display = "block";
+  document.body.classList.add('modal-open');
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   mymodal.style.display = "none";
+  document.body.classList.remove('modal-open');
 }
 
 // Request a Dish Modal
@@ -46,11 +29,13 @@ var btn2 = document.getElementById("dish");
 // When the user clicks on the button, open the modal
 btn2.onclick = function() {
   mymodal2.style.display = "block";
+  document.body.classList.add('modal-open');
 }
 
 // When the user clicks on <span> (x), close the modal
 ended.onclick = function() {
   mymodal2.style.display = "none";
+  document.body.classList.remove('modal-open');
 }
 
 
